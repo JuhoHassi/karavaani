56227d8c22bdca5438fdffd83c33c75a67d152e1
Gerli Rand <gerli.rand@student.careeria.fi>
2023-03-16 17:33:25 +0200

    Etusivun tekstin ja css muutokset

--- src/Etusivu.jsx
+++ src/Etusivu.jsx
@@ -13,11 +13,11 @@ const Etusivu = () => {
                 <span>S</span>
                 <span>T</span>
                 <span>A</span>
-            </div>
-            <div  className='etuText'>
                 <h4>vuokraamalla matkailuauto</h4>
             </div>
-           
+            {/* <div  className='etuText'>
+                <h4>vuokraamalla matkailuauto</h4>
+            </div> */}
         </div>
     )
 }
